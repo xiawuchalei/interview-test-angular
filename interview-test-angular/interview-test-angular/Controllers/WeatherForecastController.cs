22780c6982c29efe9c92623792e9961ef9ea695c
Andrew de Lange <andrew.de.lange@rezare.com>
2022-03-17 10:10:58 +1300

    Created new project based off the Visual Studio 2022 Angular Template. updated the example controller endpoint to work via medatr. Dependancy Injection still not fully implemented.

---
+++ interview-test-angular/interview-test-angular/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using interview_test_angular.Mediatr.Forecast;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace interview_test_angular.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private IMediator mediator;
+
+        /// <summary>
+        /// Gets the Mediator object.
+        /// </summary>
+        protected IMediator Mediator => mediator ??= (IMediator)HttpContext.RequestServices.GetService(typeof(IMediator))!;
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var reponse = await mediator.Send(new GetForcastRequest());
+
+            return reponse.Forecast;
+        }
+    }
+}
