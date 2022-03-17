aef62f3d069bce55098fdbb8c902913dce5e5cdf
Andrew de Lange <andrew.de.lange@rezare.com>
2022-03-18 09:06:11 +1300

    moved the angular project out of the C# solution. not yet updated the angular to find the new base URL

--- interview-test-angular/interview-test-angular/Startup.cs
+++ interview-test-angular/interview-test-angular/Startup.cs
@@ -23,11 +23,6 @@ namespace interview_test_angular
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
 
             // Register the MediatR request handlers
             services.RegisterRequestHandlers();
@@ -61,19 +56,6 @@ namespace interview_test_angular
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
             });
         }
 
