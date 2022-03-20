caa5749d9762b3348123622ba3c74d584d5c810c
Andrew de Lange <andrew.de.lange@rezare.com>
2022-03-21 11:55:19 +1300

    Added a new Mediator Handler for getting the list of students

---
+++ interview-test-angular/interview-test-angular.Mediatr/Students/GetStudentsRequest.cs
@@ -0,0 +1,33 @@
+﻿using interview_test_angular.Models.Students;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace interview_test_angular.Mediatr.Students
+{
+    public class GetStudentsRequest : IRequest<GetStudentsResponse>
+    {
+    }
+
+    public class GetStudentsResponse
+    {
+        public List<Student> Students { get; set; }
+
+        public GetStudentsResponse()
+        {
+            Students = new List<Student>();
+        }
+    }
+
+    public class GetStudentsHandler : IRequestHandler<GetStudentsRequest, GetStudentsResponse>
+    {
+        public Task<GetStudentsResponse> Handle(GetStudentsRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
